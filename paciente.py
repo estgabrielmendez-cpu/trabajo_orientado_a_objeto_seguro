a25a2dda3dfce4f9c1f6a3e92b9fe87fcd3aa4e2
estgabrielmendez-cpu <est.gabrielmendez@liceotpvioletaparra.cl>
2026-09-09 09:58:38 -0300

    paciente iniciado

---
+++ paciente.py
@@ -0,0 +1,17 @@
+class paciente:
+
+    PREVISIONES = Set(str) = ("Fonasa", "Isapre")
+
+    def __init__(self, rut:str, nombre:str, edad:int, prevision:str):
+        self.rut = rut
+        self.nombre = nombre
+        self.edad = edad
+        self.prevision = prevision
+
+        @property
+        def rut(self) -> str: 
+            return self._rut
+
+        @rut.setter
+        def rut(self, rut: str) -> None:
+            self._rut = rut
